dfb81bef555aa97f9efe94673159de6c6c493ff7
Ed Gauthier <ed@egauthier.net>
2014-01-28 08:59:14 -0500

    Handle fonts across Windows and Mac.

--- emacs.d/lisp/my-look.el
+++ emacs.d/lisp/my-look.el
@@ -2,7 +2,10 @@
 
 (if (display-graphic-p)
     (progn ;; then
+      (if (eq system-type 'darwin)
       (set-face-attribute 'default nil :font "AndaleMono-14"))
+      (if (eq system-type 'windows-nt)
+	  (set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-10.0")))
     ;; else
 )
 
