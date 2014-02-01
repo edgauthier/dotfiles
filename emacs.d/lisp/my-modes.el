83b62c08a28fed4ad0ef60df25dcc9c59d975853
Ed Gauthier <ed@egauthier.net>
2014-02-01 18:13:05 -0500

    Org mode settings

--- emacs.d/lisp/my-modes.el
+++ emacs.d/lisp/my-modes.el
@@ -1,5 +1,7 @@
 ;; org
 (setq org-log-done t)
 (setq org-agenda-files (list "~/org"))
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-mobile-inbox-for-pull "~/org/mobile-inbox.org")
 
 (provide 'my-modes)
