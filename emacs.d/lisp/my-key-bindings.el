a533b9702c20ba249aca4036972ac0570be7c6a5
Ed Gauthier <ed@egauthier.net>
2014-02-01 18:13:50 -0500

    Use a minor mode for custom key mappings

--- emacs.d/lisp/my-key-bindings.el
+++ emacs.d/lisp/my-key-bindings.el
@@ -1,11 +1,19 @@
-;; C-, bindings
-(define-prefix-command 'my-prefix-map)
-(global-set-key (kbd "C-,") 'my-prefix-map)
-(define-key my-prefix-map (kbd "b") 'my-theme-cycle)
-(define-key my-prefix-map (kbd "s") 'switch-to-scratch-and-back)
-
 ;; C-c bindings
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+
+;; C-, bindings for a minor mode
+(define-prefix-command 'my-prefix-map)
+(define-key my-prefix-map (kbd "b") 'my-theme-cycle)
+(define-key my-prefix-map (kbd "s") 'switch-to-scratch-and-back)
+
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+(define-key my-keys-minor-mode-map (kbd "C-,") 'my-prefix-map)
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+
+(my-keys-minor-mode 1)
 
 (provide 'my-key-bindings)
