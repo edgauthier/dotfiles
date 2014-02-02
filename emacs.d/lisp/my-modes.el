0dbcd82141e4bb212cc85d19fdc663cd40dced60
Ed Gauthier <ed@egauthier.net>
2014-02-02 17:49:09 -0500

    Updated org settings

--- emacs.d/lisp/my-modes.el
+++ emacs.d/lisp/my-modes.el
@@ -1,7 +1,9 @@
 ;; org
+(setq org-directory "~/org")
+(setq org-agenda-files (list (concat org-directory "/inbox.org")
+			     (concat org-directory "/misc.org")
+			     (concat org-directory "/system_maint.org")
+			     (concat org-directory "/active/")))
 (setq org-log-done t)
-(setq org-agenda-files (list "~/org"))
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(setq org-mobile-inbox-for-pull "~/org/mobile-inbox.org")
 
 (provide 'my-modes)
