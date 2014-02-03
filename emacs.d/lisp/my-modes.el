4270ca9dea1e794004e4f6f8bcb822e26c134ed2
Ed Gauthier <ed@egauthier.net>
2014-02-03 17:11:28 -0500

    Org modes settings for refile targets and special handling of C-a/C-e

--- emacs.d/lisp/my-modes.el
+++ emacs.d/lisp/my-modes.el
@@ -6,4 +6,11 @@
 			     (concat org-directory "/active/")))
 (setq org-log-done t)
 
+; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+;; Special handling for C-a/C-e
+(setq org-special-ctrl-a/e t)
+
 (provide 'my-modes)
