06282e7968ce1f6997d81419ecace4d778022f4e
Ed Gauthier <ed@egauthier.net>
2014-02-03 17:40:55 -0500

    Re-org org mode files.

--- emacs.d/lisp/my-modes.el
+++ emacs.d/lisp/my-modes.el
@@ -1,7 +1,6 @@
 ;; org
 (setq org-directory "~/org")
 (setq org-agenda-files (list (concat org-directory "/inbox.org")
-			     (concat org-directory "/misc.org")
 			     (concat org-directory "/system_maint.org")
 			     (concat org-directory "/active/")))
 (setq org-log-done t)
